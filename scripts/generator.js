208da1ba1b25b3fb5fe32f6816a204485f95ede4
Nathan Hartley <91960789+NateHartley@users.noreply.github.com>
2024-12-11 16:29:17 +0000

    Implemented logic to simplified symbols checkbox

--- scripts/generator.js
+++ scripts/generator.js
@@ -4,6 +4,7 @@ document.getElementById("chkbx1").addEventListener("click", checkbox_upper_only)
 document.getElementById("chkbx2").addEventListener("click", checkbox_lower_only);
 document.getElementById("chkbx3").addEventListener("click", checkbox_no_numbers);
 document.getElementById("chkbx4").addEventListener("click", checkbox_no_symbols);
+document.getElementById("chkbx5").addEventListener("click", checkbox_simplified_symbols);
 
 var rand = undefined;
 var compare_rand = undefined;
@@ -12,6 +13,7 @@ var upper_only = false;
 var lower_only = false;
 var no_numbers = false;
 var no_symbols = false;
+var simplified_symbols = false;
 const checkBoxes = document.querySelectorAll('input[type="checkbox"]');
 
 
@@ -26,6 +28,7 @@ const checkBoxes = document.querySelectorAll('input[type="checkbox"]');
 function gen_pwd() {
     var password = undefined;
     var pwd_array = [];
+    var simplified_symbols_list_ascii = ["!", "#", "%", "+", ":", "=", "?", "@"];
 
     while (true) {
         if (pwd_array.length == 16) {
@@ -35,6 +38,22 @@ function gen_pwd() {
         var char = gen_rand();
         var text = String.fromCharCode(char); // Ascii code to text
         pwd_array.push(text);
+    }
+
+    // If simplified symbols mode is on, but password doesn't contain any symbols, replace last char of password with symbol
+    if (simplified_symbols) {
+        var count = 0;
+        for (let j = 0; j < simplified_symbols_list_ascii.length; j++) {
+            if (pwd_array.includes(simplified_symbols_list_ascii[j]) == false) {
+                count++;
+            }
+        }
+        
+        if (count == simplified_symbols_list_ascii.length) {
+            let index = Math.floor(Math.random() * simplified_symbols_list_ascii.length);
+            pwd_array.pop();
+            pwd_array.push(simplified_symbols_list_ascii[index]);
+        }
     }
 
     password = pwd_array.join('');
@@ -47,6 +66,7 @@ function gen_rand() {
     crypto.getRandomValues(int8);
     rand = int8[0]; // Return first val of int8 array which is the random val
     var repeat = false;
+    var simplified_symbols_list_dec = [33, 35, 37, 43, 58, 61, 63, 64]; // ! # % + : = ? @
 
     // Uppercase
     if ((rand > 64 && rand < 91) && (lower_only == false)) {
@@ -66,9 +86,13 @@ function gen_rand() {
         ((rand > 57 && rand < 65) && 
         (rand != 60 && rand != 62)) || 
         (rand > 90 && rand < 97) || 
-        (rand > 122 && rand < 127)) && (no_symbols == false)) {
+        (rand > 122 && rand < 127)) && (no_symbols == false) && (simplified_symbols == false)) {
         group = 4;
-
+    
+    // Simplified symbol
+    } else if ((simplified_symbols_list_dec.includes(rand)) && (simplified_symbols) && (no_symbols == false)) {
+        group = 5;
+    
     // Out of bounds
     } else if ((rand < 33 || rand > 126) || 
         (rand == 60 || rand == 62)) {
@@ -134,17 +158,32 @@ function checkbox_no_numbers() {
 function checkbox_no_symbols() {
     var checkbox = document.getElementById("chkbx4");
     checkbox.checked ? no_symbols = true: no_symbols = false;
+    if (simplified_symbols) { simplified_symbols = false; }
+}
+
+function checkbox_simplified_symbols() {
+    var checkbox = document.getElementById("chkbx5");
+    checkbox.checked ? simplified_symbols = true: simplified_symbols = false;
+    if (no_symbols) { no_symbols = false; }
 }
 
 function handleCheckboxClick(event) {
   checkBoxes.forEach((checkBox) => {
     var upper_only_chkbx = checkBoxes[0];
     var lower_only_chkbx = checkBoxes[1];
+    var no_symbols_chkbx = checkBoxes[3];
+    var simplified_symbols_chkbx = checkBoxes[4];
 
     if (event.target == upper_only_chkbx) {
         lower_only_chkbx.checked = false;
     } else if (event.target == lower_only_chkbx) {
         upper_only_chkbx.checked = false;
+    }
+
+    if (event.target == no_symbols_chkbx) {
+        simplified_symbols_chkbx.checked = false;
+    } else if (event.target == simplified_symbols_chkbx) {
+        no_symbols_chkbx.checked = false;
     }
   });
 }
